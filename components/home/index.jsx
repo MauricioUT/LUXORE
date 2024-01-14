f1283522347930057b101264893dc6d1c282281b
raul <rstrujillod@scjn.gob.mx>
2024-01-13 19:15:24 -0600

    removing for permance home base

--- components/home/index.jsx
+++ components/home/index.jsx
@@ -1,14 +1,9 @@
-import CallToAction from "../common/CallToAction";
-import CopyrightFooter from "../common/footer/CopyrightFooter";
+
 import Footer from "../common/footer/Footer";
 import MobileMenu from "../common/header/MobileMenu";
-import Partners from "../common/Partners";
-import Blogs from "../common/Blogs";
 import FeaturedProperties from "./FeaturedProperties";
-import FindProperties from "./FindProperties";
 import Header from "./Header";
 import Hero from "./Hero";
-import WhyChoose from "../common/WhyChoose";
 import PopupSignInUp from "../common/PopupSignInUp";
 
 const Index = () => {
@@ -32,7 +27,7 @@ const Index = () => {
           <div className="row">
             <div className="col-lg-6 offset-lg-3">
               <div className="main-title text-center mb40">
-                <h2>Featured Properties</h2>
+                <h2>Featured rulas</h2>
                 <p>Handpicked properties by our team.</p>
               </div>
             </div>
@@ -45,80 +40,6 @@ const Index = () => {
         </div>
       </section>
 
-      {/* <!-- Property Cities --> */}
-      <section id="property-city" className="property-city pb30">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-6 offset-lg-3">
-              <div className="main-title text-center">
-                <h2>Find Properties in These Cities</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <FindProperties />
-          </div>
-        </div>
-      </section>
-
-      {/* <!-- Why Chose Us --> */}
-      <section id="why-chose" className="whychose_us bgc-f7 pb30">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-6 offset-lg-3">
-              <div className="main-title text-center">
-                <h2>Why Choose Us</h2>
-                <p>We provide full service at every step.</p>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <WhyChoose />
-          </div>
-        </div>
-      </section>
-
-      {/* <!-- Our Blog --> */}
-      <section className="our-blog bgc-f7 pb30">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-6 offset-lg-3">
-              <div className="main-title text-center">
-                <h2>Articles & Tips</h2>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <Blogs />
-          </div>
-        </div>
-      </section>
-
-      {/* <!-- Our Partners --> */}
-      <section id="our-partners" className="our-partners">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-6 offset-lg-3">
-              <div className="main-title text-center">
-                <h2>Our Partners</h2>
-                <p>We only work with the best companies around the globe</p>
-              </div>
-            </div>
-          </div>
-          <div className="row">
-            <Partners />
-          </div>
-        </div>
-      </section>
-
-      {/* <!-- Start Call to Action --> */}
-      <section className="start-partners bgc-thm pt50 pb50">
-        <div className="container">
-          <CallToAction />
-        </div>
-      </section>
 
       {/* <!-- Our Footer --> */}
       <section className="footer_one">
@@ -129,12 +50,6 @@ const Index = () => {
         </div>
       </section>
 
-      {/* <!-- Our Footer Bottom Area --> */}
-      <section className="footer_middle_area pt40 pb40">
-        <div className="container">
-          <CopyrightFooter />
-        </div>
-      </section>
     </>
   );
 };
