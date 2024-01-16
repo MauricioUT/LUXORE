ce81e4be877655f63b5b1da826159ef30dd17e52
raul <rstrujillod@scjn.gob.mx>
2024-01-16 17:00:03 -0600

    remove

--- components/common/listing/SidebarListing.jsx
+++ components/common/listing/SidebarListing.jsx
@@ -12,30 +12,6 @@ const SidebarListing = () => {
                 </div>
             </div>
             {/* End .sidebar_listing_list */}
-
-            <div className="terms_condition_widget">
-                <h4 className="title">Featured Properties</h4>
-                <div className="sidebar_feature_property_slider">
-                    <FeatureProperties />
-                </div>
-            </div>
-            {/* End .Featured Properties */}
-
-            <div className="terms_condition_widget">
-                <h4 className="title">Categories Property</h4>
-                <div className="widget_list">
-                    <ul className="list_details">
-                        <Categorie />
-                    </ul>
-                </div>
-            </div>
-            {/* End .Categories Property */}
-
-            <div className="sidebar_feature_listing">
-                <h4 className="title">Recently Viewed</h4>
-                <FeaturedListings />
-            </div>
-            {/* End .Recently Viewed */}
         </div>
     );
 };
--- components/home-3/Home3.jsx
+++ components/home-3/Home3.jsx
@@ -24,59 +24,8 @@ const Home3 = () => {
       <Hero />
 
     {/* <!-- Listing Grid View --> */}
-    <section className="our-listing bgc-f7 pb30-991 mt85 md-mt0 ">
+     <section className="our-listing bgc-f7 pb30-991 mt85 md-mt0 ">
         <div className="container">
-          <div className="row">
-            <div className="col-lg-6">
-              <BreadCrumb />
-            </div>
-            {/* End .col */}
-
-            <div className="col-lg-6 position-relative">
-              <div className="listing_list_style mb20-xsd tal-991">
-                <GridListButton />
-              </div>
-              {/* End list grid */}
-
-              <div className="dn db-991 mt30 mb0">
-                <ShowFilter />
-              </div>
-              {/* ENd button for mobile sidebar show  */}
-            </div>
-            {/* End .col filter grid list */}
-          </div>
-          {/* End Page Breadcrumb and Grid,List and filter Button */}
-
-          <div className="row">
-            <div className="col-md-12 col-lg-12">
-              <div className="grid_list_search_result ">
-                <div className="row align-items-center">
-                  <FilterTopBar />
-                </div>
-              </div>
-              {/* End .row */}
-
-              <div className="row">
-                <FeaturedItem />
-              </div>
-              {/* End .row */}
-
-              <div className="row">
-                <div className="col-lg-12 mt20">
-                  <div className="mbp_pagination">
-                    <Pagination />
-                  </div>
-                </div>
-                {/* End paginaion .col */}
-              </div>
-              {/* End .row */}
-            </div>
-            {/* End  .col */}
-
-            
-          </div>
-          {/* End .row */}
-
           <div className="row">
             <div className="col-lg-4 col-xl-4">
               <div className="sidebar-listing-wrapper">
@@ -90,7 +39,7 @@ const Home3 = () => {
                 id="sidebarListing"
               >
                 <div className="offcanvas-header">
-                  <h5 className="offcanvas-title">Advanced Search</h5>
+                  <h5 className="offcanvas-title">Advanced rulas</h5>
                   <button
                     type="button"
                     className="btn-close text-reset"
