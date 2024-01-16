430a60108044ce55da9f7b2185590c9ae56bcf19
MauricioUT <mauricio.utrilla.d@hotmail.com>
2024-01-15 18:27:50 -0600

    agent module was deleted

--- components/common/header/dashboard/HeaderMenuContent.jsx
+++ components/common/header/dashboard/HeaderMenuContent.jsx
@@ -105,24 +105,6 @@ const HeaderMenuContent = ({ float = "" }) => {
         },
       ],
     },
-    {
-      id: 5,
-      title: "Agent View",
-      items: [
-        {
-          name: "Agent V1",
-          routerPath: "/agent-v1",
-        },
-        {
-          name: "Agent V2",
-          routerPath: "/agent-v2",
-        },
-        {
-          name: "Agent Details",
-          routerPath: "/agent-details",
-        },
-      ],
-    },
   ];
 
   const property = [
