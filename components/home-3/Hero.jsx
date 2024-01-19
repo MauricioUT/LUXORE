bb2169f0996da90251c0cdfe9591bb5c4d2f856d
raul <rstrujillod@scjn.gob.mx>
2024-01-19 10:38:12 -0600

    change  image for black back ground

--- components/home-3/Hero.jsx
+++ components/home-3/Hero.jsx
@@ -2,12 +2,14 @@ import GlobalHeroFilter from "../common/GlobalHeroFilter";
 
 const Hero = () => {
   return (
+    // 
     <section className="home-three bg-img3 md-mt0 bg-anime">
       <div className="container">
         <div className="row">
           <div className="col-lg-8">
             <div className="home3_home_content">
-              <h1>Bienes raíces - Tu inversión, nuestra especialidad.</h1>
+              <h1>Bienes raíces </h1>
+              <h2>Tu inversión, nuestra especialidad.</h2>
               {/* <h4>
                 From as low as $10 per day with limited time offer discounts
               </h4> */}
