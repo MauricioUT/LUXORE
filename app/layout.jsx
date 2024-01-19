eea17f990166686eb05e6db92e369f858bab58c1
raul <rstrujillod@scjn.gob.mx>
2024-01-19 10:34:57 -0600

    remove google fonts

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,7 +14,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <link  rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:400,400i,500,600,700&display=swap" />
+        {/* <link  rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:400,400i,500,600,700&display=swap" /> */}
         <link  type="image/x-icon" rel="icon" href="./favicon.ico" />
       </head>
       <body >
