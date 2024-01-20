cbec5729ccb4e0c96dbef7f8ed96267b8378437e
MauricioUT <mauricio.utrilla.d@hotmail.com>
2024-01-20 13:41:03 -0600

    add header global

---
+++ components/common/header/Header.jsx
@@ -0,0 +1,60 @@
+'use client'
+
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import HeaderMenuContent from "../common/header/HeaderMenuContent";
+import Image from "next/image";
+
+const Header = () => {
+  const [navbar, setNavbar] = useState(false);
+
+  const changeBackground = () => {
+    if (window.scrollY >= 95) {
+      setNavbar(true);
+    } else {
+      setNavbar(false);
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", changeBackground);
+  }, []);
+
+  return (
+    <header
+      className={`header-nav menu_style_home_one navbar-scrolltofixed stricky main-menu  ${
+        navbar ? "stricky-fixed " : ""
+      }`}
+    >
+      <div className="container-fluid p0">
+        {/* <!-- Ace Responsive Menu --> */}
+
+        <Link href="/" className="navbar_brand float-start dn-smd">
+          <Image
+            width={100}
+            height={100}
+            className="logo1 img-fluid"
+            src="/assets/images/logoDorado1.png"
+            alt="header-logo2.png"
+          />
+          <Image
+            width={100}
+            height={100}
+            className="logo2 img-fluid"
+            src="/assets/images/logoDorado1.png"
+            alt="header-logo2.png"
+          />
+        </Link>
+        {/* site logo brand */}
+
+        <nav>
+          <HeaderMenuContent />
+        </nav>
+        {/* End .navbar */}
+      </div>
+    </header>
+    // {/* <!-- /.theme-main-menu --> */}
+  );
+};
+
+export default Header;
