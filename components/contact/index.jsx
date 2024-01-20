279515064d137db8f61cae016e9646c1db535ca7
MauricioUT <mauricio.utrilla.d@hotmail.com>
2024-01-20 13:56:11 -0600

    Comtact menu was added

--- components/contact/index.jsx
+++ components/contact/index.jsx
@@ -1,8 +1,7 @@
 import Image from "next/image";
 import Footer from "../common/footer/Footer";
-import Header from "../common/header/DefaultHeader";
+import Header from "../common/header/Header";
 import MobileMenu from "../common/header/MobileMenu";
-import AddressSidebar from "./AddressSidebar";
 import BreadCrumbBanner from "./BreadCrumbBanner";
 import Form from "./Form";
 
@@ -22,53 +21,23 @@ const index = () => {
       <section className="our-contact pb0 bgc-f7">
         <div className="container">
           <div className="row">
-            <div className="col-lg-7 col-xl-8">
+            <div className="col-lg-12 col-xl-12">
               <div className="form_grid">
-                <h4 className="mb5">Send Us An Email</h4>
+                <h4 className="mb5">Agenda una cita</h4>
                 <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. In
-                  gravida quis libero eleifend ornare. Maecenas mattis enim at
-                  arcu feugiat, sit amet blandit nisl iaculis. Donec lacus odio,
-                  malesuada eu libero sit amet, congue aliquam leo. In hac
-                  habitasse platea dictumst.
+                Evita riesgos innecesarios,
+                elige invertir en Bienes Raíces en México con una empresa confiable y transparente, 
+                respaldada por asesores con más de 12 años de experiencia en el sector.
                 </p>
                 <Form />
               </div>
             </div>
             {/* End .col */}
 
-            <div className="col-lg-5 col-xl-4">
-              <AddressSidebar />
-            </div>
           </div>
           {/* End .row */}
         </div>
         {/* End .container */}
-
-        <div className="container-fluid p0 mt50">
-          <div className="row">
-            <div className="col-lg-12">
-              <div className="h600" id="map-canvas">
-                <div className="gmap_canvas pe-none">
-                  <iframe
-                    title="map"
-                    className="gmap_iframe"
-                    src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d193309.02147838814!2d-74.53513266718751!3d40.79602810000002!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2sbd!4v1663993365939!5m2!1sen!2sbd"
-                  ></iframe>
-                  {/* End iframe */}
-
-                  <Image
-                    width={32}
-                    height={50}
-                    className="location-finder"
-                    src="/assets/images/location.png"
-                    alt="location"
-                  />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
       </section>
 
 
