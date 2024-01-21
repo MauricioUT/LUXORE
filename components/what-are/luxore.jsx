263ec6a6a8cbccafa4d9b60bfc0c5f26f8199220
MauricioUT <mauricio.utrilla.d@hotmail.com>
2024-01-20 18:59:44 -0600

    what are was created

---
+++ components/what-are/luxore.jsx
@@ -0,0 +1,122 @@
+import Image from "next/image";
+
+const Luxore = () => {
+
+
+  return (
+    <>
+      <div className="row">
+        <div className="col-lg-4 col-xl-5">
+          <div className="about_thumb" style={{height:'100%'}}>
+            <Image style={{height:'100%'}}
+              width={461}
+              height={600}
+              priority
+              className="img-fluid w100 cover"
+              src="/assets/images/luxore/bussinesman-smile-img.webp"
+              alt="1.jpg"
+            />
+          </div>
+        </div>
+        <div className="col-lg-8 col-xl-7">
+          <div className="what-are-content">
+            <h3>Bienes raíces</h3>
+            <p >
+              Hacen referencia a los activos físicos que incluyen la tierra y cualquier <strong>estructura</strong> o edificación que se encuentre en ella.
+              Estas propiedades están vinculadas al suelo y no se pueden mover de un lugar a otro.
+            </p>
+            <h4>Las propiedades inmobiliarias pueden clasificarse en varios tipos:</h4>
+            <p>
+              <strong>Residenciales:</strong> Hogares, apartamentos, casas, etc.
+            </p>
+            <p>
+              <strong>Comerciales:</strong> Edificios destinados para actividades comerciales, como oficinas, tiendas, hoteles, etc.
+            </p>
+            <p>
+              <strong>Industriales:</strong> Espacios para actividades de manufactura, almacenamiento y distribución.
+            </p>
+            <p>
+              <strong>Terrenos:</strong> Parcelas de tierra sin edificar, listas para su desarrollo.
+            </p>
+            <p>
+              <strong>Agrícolas:</strong> Terrenos destinados a la agricultura y producción de alimentos.
+            </p>
+          </div>
+        </div>
+      </div>
+      {/* End .col */}
+      <div className="row">
+        <div className="col-lg-8 col-xl-7">
+          <div className="what-are-content">
+            <h3>Las propiedades inmobiliarias pueden clasificarse en varios tipos:</h3>
+            <p>
+              <strong>Potencial de apreciación del capital:</strong> A lo largo del tiempo, los bienes inmuebles tienden a aumentar de valor. Esta apreciación puede generar ganancias a largo plazo.
+            </p>
+            <p>
+              <strong>Flujo de efectivo:</strong> Si decides alquilar la propiedad, puedes obtener ingresos regulares en forma de alquiler.
+            </p>
+            <p>
+              <strong>Inflación y protección del valo:</strong> Los bienes raíces a menudo actúan como una protección contra la inflación, ya que los precios de las propiedades tienden a aumentar con la inflación.
+            </p>
+            <p>
+              <strong>Herencia y legado:</strong> Los bienes raíces se pueden mantener y transmitir a futuras generaciones, formando parte de tu patrimonio familiar.
+            </p>
+          </div>
+        </div>
+        <div className="col-lg-4 col-xl-5">
+          <div className="about_thumb">
+            <Image
+              width={461}
+              height={600}
+              priority
+              className="img-fluid w100 cover"
+              src="/assets/images/luxore/ourhistory.jpg"
+              alt="1.jpg"
+            />
+          </div>
+        </div>
+      </div>
+      {/* End .col */}
+
+      <div className="row">
+        <div className="col-lg-4 col-xl-5">
+          <div className="about_thumb">
+            <Image
+              width={461}
+              height={600}
+              priority
+              className="img-fluid w100 cover"
+              src="/assets/images/luxore/bussinesman-smile-img.webp"
+              alt="1.jpg"
+            />
+          </div>
+        </div>
+        <div className="col-lg-8 col-xl-7">
+          <div className="what-are-content">
+            <h3>¿Cuáles son las etapas de compra de un inmueble?</h3>
+            <p>
+              Búsqueda y Selección
+            </p>
+            <p>
+              Negociación y Oferta
+            </p>
+            <p>
+              Inspección y Evaluación
+            </p>
+            <p>
+              Financiamiento o Capitalización
+            </p>
+            <p>
+              Cierre de Compra
+            </p>
+            <p>
+              Es fundamental contar con la asesoría de un agente inmobiliario y, en muchos casos, un abogado especializado en transacciones inmobiliarias para guiar y facilitar estos procesos.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Luxore;
