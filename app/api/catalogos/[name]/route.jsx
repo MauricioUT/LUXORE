c2789ea6b4f71ca25054895a816df819dfbe87a0
raul <rstrujillod@scjn.gob.mx>
2024-01-29 13:04:09 -0600

    change to gcloud mysql

--- app/api/catalogos/[name]/route.jsx
+++ app/api/catalogos/[name]/route.jsx
@@ -4,8 +4,10 @@ import { conn } from '@/libs/mysql'
 export async function GET(request, { params }){ 
    const cat = ["C_CATEGORIES", "C_AMENITIES", "C_STATES", "C_PROPERTY_TYPES"];
    let results = {message: "No se encontro el catalogo"};
-   if(cat.includes(params.name))
+   if(cat.includes(params.name)){
       results = await conn.query(`SELECT * FROM ${params.name}`);
+      conn.end(); // Cerrar la conexión después de la consulta
+   }
 
    return NextResponse.json(results);
 }
--- libs/mysql.jsx
+++ libs/mysql.jsx
@@ -2,10 +2,25 @@ import mysql from 'serverless-mysql'
 
 export const conn = mysql({
     config:{
-        host:'localhost',
+        host:'130.211.237.191',
         database:'luxore',
         port:3306,
         user:'root',
-        password: 'MY$QL2023'
+        password: 'lUx20230849'
     }
 })
+
+
+
+// // Función para ejecutar consultas
+// export const query = (sql, params = []) => {
+//     // return new Promise((resolve, reject) => {
+//     //     conn.query(sql, params, (err, results) => {
+//     //     if (err) reject(err);
+//     //     else{
+//     //     connection.end(); // Cerrar la conexión después de la consulta
+//     //     resolve(results);
+//     //     }
+//     //   });
+//     // });
+//   };
