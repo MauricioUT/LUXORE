4118fef5188566484c4c654892b5ae77be493d96
raul <rstrujillod@scjn.gob.mx>
2024-02-05 20:22:34 -0600

    add description to the main page

--- app/page.jsx
+++ app/page.jsx
@@ -2,7 +2,9 @@ import Wrapper from "@/components/layout/Wrapper";
 import HomeMain from './(homes)/home-3/page'
 
 export const metadata = {
-  title: 'Luxore',
+  title: 'Luxore - Capital                      ',
+  description:
+  'Desarrollador inmobiliario especializado en la venta de bienes raíces. ',
 }
 
 export default function Home() {
