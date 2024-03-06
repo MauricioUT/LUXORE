cf073c539ae5cbd2c20f599810320080efb85014
MauricioUT <mauricio.utrilla.d@hotmail.com>
2024-03-05 18:07:12 -0600

    onclick in image was added

--- components/home-3/FeaturedItem.jsx
+++ components/home-3/FeaturedItem.jsx
@@ -2,9 +2,9 @@
 'use client'
 
 import Link from "next/link";
-import { useEffect, useState  } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { addLength,addSkip,addCurrentPage } from "../../features/properties/propertiesSlice";
+import { addLength, addSkip, addCurrentPage } from "../../features/properties/propertiesSlice";
 import Image from "next/image";
 import ReactPaginate from 'react-paginate';
 import { NumericFormat } from 'react-number-format';
@@ -17,16 +17,16 @@ const FeaturedItem = () => {
     skip
   } = useSelector((state) => state.properties);
 
-  
+
   const { isGridOrList } = useSelector(
     (state) => state.filter
   );
- 
 
-  
+
+
   //paginator constants
   const [getSkip, setSkip] = useState(skip);
-  const pageCount = Math.ceil(count /9);
+  const pageCount = Math.ceil(count / 9);
 
 
   const dispatch = useDispatch();
@@ -36,45 +36,46 @@ const FeaturedItem = () => {
     .map((item) => (
 
       <div
-        className={`${
-          isGridOrList ? "col-12 feature-list" : "col-md-4 col-lg-4"
-        } `}
+        className={`${isGridOrList ? "col-12 feature-list" : "col-md-4 col-lg-4"
+          } `}
         key={item.id}
       >
         <div
-          className={`feat_property home7 style4 ${
-            isGridOrList ? "d-flex align-items-center" : undefined
-          }`}
-        >  
-          <div className="thumb">
-        
-            <Image
-              width={342}
-              height={220}
-              className="img-whp w-100 h-100 cover"
-              src={item.mainImage}
-              alt="fp1.jpg"
-            />
-            <div className="thmb_cntnt">
-              <ul className="tag mb0">
-              {item.featuredProperty == 1 &&<li className="list-inline-item">
-                 <a href="#">Propiedad destacada</a>
-                </li>}
-                <li className="list-inline-item">
-                  <a href="#" className="text-capitalize">
-                    {item.category}
-                  </a>
-                </li>
-              </ul>
-              <Link
-                href={`/detalle/${item.id}`}
-                className="fp_price"
-              >
-                  <NumericFormat value={item?.price} displayType={'text'} thousandSeparator={true} prefix={'$'} />       
+          className={`feat_property home7 style4 ${isGridOrList ? "d-flex align-items-center" : undefined
+            }`}
+        >
+          <Link href={`/detalle/${item.id}`}>
+
+            <div className="thumb">
+
+              <Image
+                width={342}
+                height={220}
+                className="img-whp w-100 h-100 cover"
+                src={item.mainImage}
+                alt="fp1.jpg"
+              />
+              <div className="thmb_cntnt">
+                <ul className="tag mb0">
+                  {item.featuredProperty == 1 && <li className="list-inline-item">
+                    <a href="#">Propiedad destacada</a>
+                  </li>}
+                  <li className="list-inline-item">
+                    <a href="#" className="text-capitalize">
+                      {item.category}
+                    </a>
+                  </li>
+                </ul>
+                <Link
+                  href={`/detalle/${item.id}`}
+                  className="fp_price"
+                >
+                  <NumericFormat value={item?.price} displayType={'text'} thousandSeparator={true} prefix={'$'} />
                   <small>/mx</small>
-              </Link>
+                </Link>
+              </div>
             </div>
-          </div>
+          </Link>
           <div className="details">
             <div className="tc_content">
               <p className="text-thm">{item.propertyType}</p>
@@ -90,13 +91,13 @@ const FeaturedItem = () => {
 
               <div className="row">
                 <div className="col-3">
-                <p>Recamaras:{item.bedrooms} </p>
+                  <p>Recamaras:{item.bedrooms} </p>
                 </div>
                 <div className="col-3">
-                <p>Baños:{item.bathrooms} </p>
+                  <p>Baños:{item.bathrooms} </p>
                 </div>
                 <div className="col-3">
-                <p>Metros:{item.metersSurface}m2 </p>
+                  <p>Metros:{item.metersSurface}m2 </p>
                 </div>
               </div>
             </div>
@@ -111,7 +112,7 @@ const FeaturedItem = () => {
 
       </div>
     ));
-   
+
   // add length of filter items
   useEffect(() => {
     dispatch(addLength(content.length));
@@ -119,52 +120,52 @@ const FeaturedItem = () => {
 
 
   //paginador 
-  
+
   useEffect(() => {
     dispatch(addSkip(getSkip))
-  },[getSkip]);
+  }, [getSkip]);
 
   useEffect(() => {
     setSkip(skip)
-  },[skip]);
+  }, [skip]);
 
   const handlePageClick = (event) => {
-    const itemsPerPage =  9;
-     const length = count;
-     let _take= 9;
-     let _skipt = 0;
-     let _CurrenPage= 0;
+    const itemsPerPage = 9;
+    const length = count;
+    let _take = 9;
+    let _skipt = 0;
+    let _CurrenPage = 0;
     _take = (event.selected * itemsPerPage) % length;
-    _take =  _take + itemsPerPage;
-    _skipt =  _take - itemsPerPage;
-    _CurrenPage =  _skipt/itemsPerPage;
-  console.log(
-    `Skipt ${_skipt}, take ${_take}  currentPage ${_CurrenPage}  `
-  );
+    _take = _take + itemsPerPage;
+    _skipt = _take - itemsPerPage;
+    _CurrenPage = _skipt / itemsPerPage;
+    console.log(
+      `Skipt ${_skipt}, take ${_take}  currentPage ${_CurrenPage}  `
+    );
 
-  dispatch(addCurrentPage(_CurrenPage));
-  console.log(skip);
-  console.log(getSkip);
-  setSkip(_skipt);
-};
+    dispatch(addCurrentPage(_CurrenPage));
+    console.log(skip);
+    console.log(getSkip);
+    setSkip(_skipt);
+  };
 
 
 
-  let paginador =  
-  <div className="row">
-    <div className="col-lg-6 mt20">
-      <div className="mbp_pagination">
+  let paginador =
+    <div className="row">
+      <div className="col-lg-6 mt20">
+        <div className="mbp_pagination">
           <ReactPaginate
-                containerClassName="pagination"
-                activeClassName="active"
-                pageClassName="page-item"
-                pageLinkClassName="page-link"
-                previousClassName="page-item"
-                previousLinkClassName="page-link"
-                nextClassName="page-item"
-                nextLinkClassName="page-link"
-                breakClassName="page-item"
-                breakLinkClassName="page-link"
+            containerClassName="pagination"
+            activeClassName="active"
+            pageClassName="page-item"
+            pageLinkClassName="page-link"
+            previousClassName="page-item"
+            previousLinkClassName="page-link"
+            nextClassName="page-item"
+            nextLinkClassName="page-link"
+            breakClassName="page-item"
+            breakLinkClassName="page-link"
             breakLabel="..."
             nextLabel="Siguiente >"
             onPageChange={handlePageClick}
@@ -173,15 +174,15 @@ const FeaturedItem = () => {
             previousLabel="< Anterior"
             renderOnZeroPageCount={null}
             forcePage={currentPage}
-            />
+          />
+        </div>
       </div>
-  </div>
-</div>
+    </div>
 
 
   return <>{content}  {paginador}
-           
-    </>;
+
+  </>;
 };
 
 export default FeaturedItem;
