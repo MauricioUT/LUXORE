3bcee2c9477ef39265a62a355ee51c626e6ac349
MauricioUT <mauricio.utrilla.d@hotmail.com>
2024-03-18 17:11:43 -0600

    pages wad added

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,14 @@
 import Wrapper from "@/components/layout/Wrapper";
-import HomeMain from './(homes)/home-3/page'
+import Home3 from "@/components/home-3/Home3";
 import Contact from "@/components/contact";
+import RealState from "@/components/services/real-state";
+import Thanks from "@/components/thanks";
+import HowWork from "@/components/how-work";
+import GoalAttemp from "@/components/services/goal-attempt";
+import Faq from "@/components/faq";
+import WhatAre from "@/components/what-are";
+import HowSafe from "@/components/how-safe";
+import AboutUs from "@/components/about-us";
 
 export const metadata = {
   title: 'Luxore - Capital                      ',
@@ -14,17 +22,17 @@ export const metadata = {
 
 export default function Home() {
   return (
-    <>
-     <HomeMain/>
-     <Contact/>
-     {/* <Wrapper>
-        <HomeMain/>
-        </Wrapper>
-            */}
-    </>
-
-
-  
-    
+    <Wrapper>
+      <Home3/>
+      <Contact />
+      <RealState />
+      <Thanks />
+      <HowWork />
+      <GoalAttemp />
+      <Faq />
+      <WhatAre />
+      <HowSafe />
+      <AboutUs />
+    </Wrapper>    
   )
 }
