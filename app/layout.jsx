ae2cf5677b60e2017b1eb000ee55c4ed8125b3c7
MauricioUT <mauricio.utrilla.d@hotmail.com>
2024-04-10 17:55:19 -0600

    tags GTM were  changed

--- app/layout.jsx
+++ app/layout.jsx
@@ -15,18 +15,29 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+        <head>
+          <script dangerouslySetInnerHTML={{
+            __html: `(function (w, d, s, l, i) {
+                    w[l] = w[l] || []; w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
+                    var f = d.getElementsByTagName(s)[0], j = d.createElement(s), dl = l != 'dataLayer' ? '&l=' + l : '';
+                    j.async = true; j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl; f.parentNode.insertBefore(j, f);
+                })(window, document, 'script', 'dataLayer', 'GTM-PZPFCFBK');`
+          }}></script>
+        </head>
         {/* <link  rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:400,400i,500,600,700&display=swap" /> */}
         <meta name="google-site-verification" content="sKdtPF8a1KsZr1GKPyxbLh3PNWsxnwbBbo3mVY_EZGc" />
-        <link  type="image/x-icon" rel="icon" href="./favicon.ico" />
-        <GoogleTagManager gtmId='GTM-12345' />
+        <link type="image/x-icon" rel="icon" href="./favicon.ico" />
       </head>
       <body >
-      
-      <Provider store={store}>
-      {children}
-      </Provider>
+        <noscript dangerouslySetInnerHTML={{
+          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PZPFCFBK" height="0" width="0" style="display:none;visibility:hidden"></iframe>`
+        }}></noscript>
 
-      <ScrollToTop />
+        <Provider store={store}>
+          {children}
+        </Provider>
+
+        <ScrollToTop />
       </body>
     </html>
   )
